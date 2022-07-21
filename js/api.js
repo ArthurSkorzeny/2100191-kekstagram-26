9b3192befd295b12ce05d1bb5527ff143dbd91d5
ArthurSkorzeny <104072078+ArthurSkorzeny@users.noreply.github.com>
2022-07-21 18:09:15 +0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const getData = (onSuccess, onFail) => {
-  fetch('https://26.javascript.pages.academy/kekstagram/data')
+  fetch('https://26.javascript.pages.academy/kekstagram/dat')
     .then((response) => response.json())
     .then((photos) => {
       onSuccess(photos);
